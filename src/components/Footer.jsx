e4877d0ea93bd2a9cc50de48431342d82bac37a2
Mario Polchert <mario.polchert@gmail.com>
2023-07-16 20:27:47 +0200

    build proba

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,4 +12,4 @@ const Footer = () => {
 
 export default Footer;
 
-// <footer className="absolute z-100 portrait:pb-4 portrait:mt-0 landscape:md:pb-0 landscape:md:pt-0 w-full h-[5vh] bg-red-700 flex justify-center box-border">
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,6 @@ export default {
                 md: "768px",
                 lg: "1024px",
                 xl: "1280px",
-   
             },
             container: {
                 center: true,
